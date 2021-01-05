4823eb7e058499530b2e2aff8dab12af402d8400
Tsukasa311 <70062121+Tsukasa311@users.noreply.github.com>
2021-01-05 18:11:50 +0900

    バリデーションの設定の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i
+  VALID_LAST_NAME_KANA = /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/
+  VALID_FIRST_NAME_KANA = /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/
+
   validates :nickname, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true ,format: { with: VALID_LAST_NAME_KANA }
+  validates :first_name_kana, presence: true, format: { with: VALID_FIRST_NAME_KANA}
   validates :birthday,presence: true
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 
 end
