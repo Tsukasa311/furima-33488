10fc98e5b596243c00efb90e0679461cb6846c71
Tsukasa311 <70062121+Tsukasa311@users.noreply.github.com>
2021-01-06 14:38:50 +0900

    アクティブストレージ関連導入、new,createアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,20 @@
 class ItemsController < ApplicationController
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :title,:text,:category_id, :state_id, :burden_id, :prefecture_id, :day_id, :charge).merge(id: current_user.id)
 end
