ae391198ff0d9639570712ea3e217eb2ca1703b6
Tsukasa311 <70062121+Tsukasa311@users.noreply.github.com>
2021-01-06 15:52:20 +0900

    各モデルのハッシュ記述、バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,23 @@
 class Item < ApplicationRecord
   belongs_to :User
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :burden
+  belongs_to :prefecture
+  belongs_to :day
+
+  with_options presence: true do
+    validates :title
+    validates :text
+    validates :charge
+  end
+  with_options numericality: { other_than: 1 } do
+    validates :category
+    validates :state
+    validates :burden
+    validates :prefecture
+    validates :day
+  end
 end
