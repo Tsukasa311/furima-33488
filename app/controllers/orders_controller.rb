33e9639a48ded400c0b86c2c3dbe8f7fd970f58c
Tsukasa311 <70062121+Tsukasa311@users.noreply.github.com>
2021-01-11 10:52:51 +0900

    売却済みの商品購入の制限

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
 
   def index
-    if current_user.id == @item.user_id
+    if (current_user.id == @item.user_id) || @item.order.present?
       redirect_to root_path
     end
     @order_item = OrderItem.new
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,4 @@
-class OrderItem
+class OrderItem 
   include ActiveModel::Model
 
   attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phone_number ,:user_id, :item_id, :token
